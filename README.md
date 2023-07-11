3473dc765aa5b5a55fae7afd6ada4c6758b6054c
Willy De la Cruz Sierra <willy.delacruz@protonmail.com>
2023-07-11 20:52:16 +0200

    Adding README file and an additionals API tests

---
+++ README.md
@@ -0,0 +1,21 @@
+## Running Web UI Tests
+
+```bash
+npm e2e:web
+```
+
+## Running API Tests
+
+```bash
+npm e2e:api
+```
+
+## Running all tests
+
+```bash
+npm test
+```
+
+## HTML Report
+An HTML report is generated after each test run. The report is located in the `reports` folder.
+
--- cypress/e2e/api.spec.cy.js
+++ cypress/e2e/api.spec.cy.js
@@ -1,5 +1,5 @@
 describe('Testing HTTP code statuses', () => {
-    it('should forbid requests without proper auth token', () => {
+    it('should return 403 if no auth token is provided', () => {
         cy.request({
             method: 'GET',
             failOnStatusCode: false,
@@ -10,10 +10,27 @@ describe('Testing HTTP code statuses', () => {
         });
     });
 
-    it('should return 200', () => {
+    it('should return 400 when token is incorrect', () => {
         cy.request({
             method: 'GET',
-            url: 'https://api.football-data.org/v4/matches',
+            headers: {
+                'X-Auth-Token': '8740625ef74847a7b0fb985927dabc1d0'
+            },
+            failOnStatusCode: false,
+            url: 'https://api.football-data.org/v4/competitions/SA/scorers',
+        }).as('getMatchesRequest');
+        cy.get('@getMatchesRequest').then(todos => {
+            expect(todos.status).to.eq(400);
+        });
+    });
+
+    it('should return 200 when token is correct', () => {
+        cy.request({
+            method: 'GET',
+            headers: {
+                'X-Auth-Token': '8740625ef74847a7b0fb985927dabc1d'
+            },
+            url: 'https://api.football-data.org/v4/competitions/SA/scorers',
         }).as('getMatchesRequest');
         cy.get('@getMatchesRequest').then(todos => {
             expect(todos.status).to.eq(200);
